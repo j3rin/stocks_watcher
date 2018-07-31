ea70623d728e848575520e7fb2e5eb74f5c5409a
vdevofficial <vdevofficial@gmail.com>
2018-07-31 13:36:36 -0400

    added total price

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var axios = require("axios");
 var prompt = require('prompt');
 let symbol = []
 let refreshTime = 60000;
+let myholding = 63
+
 prompt.start();
 
 prompt.get(['Symbol', 'WatchPrice'], function (err, result) {
@@ -13,11 +15,13 @@ prompt.get(['Symbol', 'WatchPrice'], function (err, result) {
   axios.get('https://api.robinhood.com/quotes/'+symbol+'/')
   .then((symbol_data) => {
      console.log(symbol_data.data.ask_price+" => "+symbol)
+     console.log("Sell Now $"+symbol_data.data.ask_price*myholding)
       if(symbol_data.data.ask_price > watchPrice){
           console.log("You making money on "+symbol+" Price "+symbol_data.data.ask_price)   
       }
   })
   .catch(function(error){
+    if(error) console.log("There was an error")
    if(error.response.status === 404){
        console.log("INVALID SYMBOL")
    }
@@ -29,11 +33,13 @@ prompt.get(['Symbol', 'WatchPrice'], function (err, result) {
         axios.get('https://api.robinhood.com/quotes/'+symbol+'/')
         .then((symbol_data) => {
             console.log(symbol_data.data.ask_price+" => "+symbol)
+            console.log("Sell Now $"+symbol_data.data.ask_price*myholding)
             if(symbol_data.data.ask_price > watchPrice){
                 console.log("You making money on "+symbol+"Price "+symbol_data.data.ask_price)   
             }
         })
         .catch(function(error){
+            if(error) console.log("There was an error")
             if(error.response.status === 404){
                 console.log("INVALID SYMBOL")
             }
